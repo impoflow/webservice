770727b572ae120e8e55a142ce7c1d2544bb2609
Ricardo <tu-correo@ejemplo.com>
2025-01-13 13:37:58 +0000

    Code refactor

--- server/api_handler.py
+++ server/api_handler.py
@@ -1,68 +1,68 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from query_handler import LambdaQueryHandler
+from query_handler import QueryHandlerFactory
 
 app = Flask(__name__)
-query_handler = LambdaQueryHandler()
+query_handler = QueryHandlerFactory().create_query_handler("lambda")
 CORS(app)
 
 @app.route('/users', methods=['GET'])
 def users():
-    response = query_handler.call("neo4j_query_handler_function", request.data)
+    response = query_handler.call(request.data)
     return jsonify(response)
 
 @app.route('/user/<string:user_id>', methods=['GET'])
 def user(user_id):
     payload = {"user_name": user_id}
-    response = query_handler.call("neo4j_query_handler_function", payload)
+    response = query_handler.call(payload)
     return jsonify(response)
 
 @app.route('/user/<string:user_id>/projects', methods=['GET'])
 def user_projects(user_id):
     payload = {"user_name": user_id, "relationship": "OWNS"}
-    response = query_handler.call("neo4j_query_handler_function", payload)
+    response = query_handler.call(payload)
     return jsonify(response)
 
 @app.route('/user/<string:user_id>/collaborations', methods=['GET'])
 def user_collaborations(user_id):
     payload = {"user_name": user_id, "relationship": "COLLABORATES"}
-    response = query_handler.call("neo4j_query_handler_function", payload)
+    response = query_handler.call(payload)
     return jsonify(response)
 
 @app.route('/user/<string:user_id>/collaborators', methods=['GET'])
 def user_collaborators(user_id):
     payload = {"user_name": user_id, "relationship": "COLLABORATES"}
-    response = query_handler.call("neo4j_query_handler_function", payload)
+    response = query_handler.call(payload)
     return jsonify(response)
 
 @app.route('/projects', methods=['GET'])
 def projects():
-    response = query_handler.call("neo4j_query_handler_function", request.data)
+    response = query_handler.call(request.data)
     return jsonify(response)
 
 @app.route('/project/<string:project_id>', methods=['GET'])
 def project(project_id):
     payload = {"project_name": project_id}
-    response = query_handler.call("neo4j_query_handler_function", payload)
+    response = query_handler.call(payload)
     return jsonify(response)
 
 @app.route('/project/<string:project_id>/owner', methods=['GET'])
 def project_owner(project_id):
     payload = {"project_name": project_id, "relationship": "OWNER"}
-    response = query_handler.call("neo4j_query_handler_function", payload)
+    response = query_handler.call(payload)
     return jsonify(response)
 
 @app.route('/project/<string:project_id>/classes', methods=['GET'])
 def project_classes(project_id):
     payload = {"project_name": project_id, "relationship": "CONTAINS"}
-    response = query_handler.call("neo4j_query_handler_function", payload)
+    response = query_handler.call(payload)
     return jsonify(response)
 
 @app.route('/project/<string:project_id>/collaborators', methods=['GET'])
 def project_collaborators(project_id):
     payload = {"project_name": project_id, "relationship": "COLLABORATES"}
-    response = query_handler.call("neo4j_query_handler_function", payload)
+    response = query_handler.call(payload)
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- server/query_handler.py
+++ server/query_handler.py
@@ -1,22 +1,38 @@
 import boto3
+from abc import ABC, abstractmethod
 
 
-class QueryHandler:
+lambda_function_name = "neo4j_query_handler_function"
+
+
+class QueryHandler(ABC):
     def __init__(self):
         pass
 
-    def call(*args, **kwargs):
+    @abstractmethod
+    def call(self, function_name, payload):
         pass
+
 
 class LambdaQueryHandler(QueryHandler):
-    def __init__(self):
-        self.lambda_client = boto3.client("lambda")
-    
-    def call(self, function_name, payload):
-        response = self.lambda_client.invoke(
-            FunctionName=function_name,
-            InvocationType="RequestResponse",
+    def __init__(self, function_name):
+        self.client = boto3.client('lambda')
+
+    def call(self, payload):
+        response = self.client.invoke(
+            FunctionName=self.function_name,
+            InvocationType='RequestResponse',
             Payload=payload
         )
-        return response
+        return response['Payload'].read()
+    
 
+class QueryHandlerFactory:
+    def __init__(self):
+        pass
+
+    def create_query_handler(self, type):
+        if type == "lambda":
+            return LambdaQueryHandler(lambda_function_name)
+        else:
+            return None
