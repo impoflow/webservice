08d3a407d1816fae88b23f57d58b0fe05bfd0e24
Óscar Rico Rodríguez <oscar.rico101@alu.ulpgc.es>
2025-01-25 15:37:53 +0000

    fix internet connection

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://api_handler:5000';
-const FILE_UPLOAD_BASE_URL = 'http://file_handler:5001';
+const API_BASE_URL = 'http://localhost:5000';
+const FILE_UPLOAD_BASE_URL = 'http://localhost:5001';
 
 document.addEventListener("DOMContentLoaded", () => {
     showHome();
